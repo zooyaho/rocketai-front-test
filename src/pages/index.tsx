7bfb4d9418e512f3825d0389ebf78e296c89a4d7
zooyaho <jiwoo1268@naver.com>
2025-05-28 12:43:51 +0900

    feat: 배경 이미지 추가 및 기본 레이아웃 완성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="relative w-full max-w-md mx-auto">HOME</div>;
+  return (
+    <div className="relative w-full max-w-md mx-auto sm:border-x">
+      {/* 배경 이미지 */}
+      <Image
+        src="/images/layer_1.png"
+        alt="사주팔자 웹툰 이미지"
+        width={448}
+        height={1400}
+        priority
+      />
+    </div>
+  );
 }
